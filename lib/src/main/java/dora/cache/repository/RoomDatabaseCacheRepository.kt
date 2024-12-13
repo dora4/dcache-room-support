315afaa83629df67ae8ce5c5df74e58555edcc97
dora <dora924666990@gmail.com>
2024-12-14 03:27:46 +0800

    Optimize the code to simplify the implementation logic.

--- lib/src/main/java/dora/cache/repository/RoomDatabaseCacheRepository.kt
+++ lib/src/main/java/dora/cache/repository/RoomDatabaseCacheRepository.kt
@@ -1,16 +1,42 @@
 package dora.cache.repository
 
 import android.content.Context
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import dora.cache.RoomCacheHolderFactory
 
 abstract class RoomDatabaseCacheRepository<T : Any, D : RoomDatabase>(context: Context)
     : BaseSuspendDatabaseCacheRepository<T, RoomCacheHolderFactory<T, D>>(context) {
 
-    abstract fun getRoomDatabase() : D
+// Rewrite it to specify the type of the model.
+// 简体中文：重写它以指定model的类型。
+// override fun getModelType(): Class<T> {
+//     return super.getModelType()
+// }
 
     /**
-     * 返回RoomDatabase的getDao的方法名。
+     * Rewrite it to specify the type of the Room database.
+     * 简体中文：重写它以指定room数据库的类型。
+     */
+    abstract fun getDatabaseType() : Class<D>
+
+    /**
+     * Rewrite it to specify the name of the database.
+     * 简体中文：重写它以指定数据库的名称。
+     */
+    abstract fun getDatabaseName() : String
+
+    /**
+     * Rewrite it to specify the method name of [RoomDatabase]'s getDao.
+     * 简体中文：重写它以指定[RoomDatabase]的getDao的方法名。
      */
     abstract fun getDaoName() : String
+
+    private fun getRoomDatabase() : D {
+        return Room.databaseBuilder(context, getDatabaseType(), getDatabaseName()).build()
+    }
+
+    override fun createCacheHolderFactory(): RoomCacheHolderFactory<T, D> {
+        return RoomCacheHolderFactory<T, D>(getRoomDatabase(), getDatabaseName())
+    }
 }
