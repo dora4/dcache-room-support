797bc98fc754403c5f8d31cc96147bae599ef051
dora <dora924666990@gmail.com>
2025-04-12 00:41:52 +0800

    Upgrade Room to version 2.4.x to support schema migrations.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -27,12 +27,12 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.github.dora4:dcache-android:3.2.7")
+    implementation("com.github.dora4:dcache-android:3.3.12")
 
     val kotlin_coroutine_version = "1.8.1"
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlin_coroutine_version")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlin_coroutine_version")
-    val room_version = "2.2.0-rc01"
+    val room_version = "2.4.3"
     api("androidx.room:room-runtime:$room_version")
     api("androidx.room:room-ktx:$room_version")
 //    annotationProcessor("androidx.room:room-compiler:$room_version")
@@ -50,7 +50,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dcache-room-support"
-                version = "1.12"
+                version = "1.13"
             }
         }
     }
